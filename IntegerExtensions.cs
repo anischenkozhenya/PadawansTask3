b1f7141eb652e983a1de619168903eee7480bc7d
Евгений Анищенко <anischenkozhenya@gmail.com>
2019-02-16 21:02:18 +0300

    Update IntegerExtensions.cs

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -6,8 +6,27 @@ namespace PadawansTask3
     {
         public static int Gcd(int a, int b)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (a == 0 || b == 0)
+            {
+                throw new NotImplementedException();
+            }
+            int gcd = 1;
+            int min;
+            if (Math.Abs(a) >= Math.Abs(b))
+            {
+                min = Math.Abs(b);
+            }
+            else
+                min = Math.Abs(a);
+
+            for (int i = 1; i <= min; i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    gcd = i;
+                }
+            }
+            return gcd;
         }
     }
 }
